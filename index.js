8479c7480472c96b47cdc558e4329089d83d304c
Maraah Sang <maraahsang@gmail.com>
2024-04-16 22:32:30 +0000

    Call operate() function to update display with operation solution

--- index.js
+++ index.js
@@ -10,61 +10,56 @@ let didEquals = false
 function displayResult(operator) { 
   if (!storedNumber) { 
     storedNumber = display.textContent  
-  } 
-  else { 
+  } else { 
     storedNumber = operate(Number(storedNumber), clickedOperator, Number(display.textContent))
       } 
   clickedOperator = operator  
   newInput = true 
   didEquals = false 
-} 
+  } 
 
 function add(a, b) {
     return a + b
   }
   
-  function subtract(a, b) {
+function subtract(a, b) {
     return a - b
   }
   
-  function multiply(a, b) {
+function multiply(a, b) {
     return a * b
   }
   
-  function divide(a, b) {
+function divide(a, b) {
     return a / b
   }
   
-  function modulo(a, b) {
+function modulo(a, b) {
     return a % b
   }
 
   function operate(a, operator, b) {
     if (operator == '+') {
       return add(a, b)   
-    }
-    else if (operator == '-') {
+    } else if (operator == '-') {
       return subtract(a, b)
-    }
-    else if (operator == 'x') {
+    } else if (operator == 'x') {
       return multiply(a, b) 
-    }
-    else if (operator == '/') {
+    } else if (operator == '/') {
         return divide(a, b)
-      } else if (operator == '%') {
+    } else if (operator == '%') {
         return modulo(a, b)
-      }
-    else {
+    } else {
       return null; 
-    } 
+    }   
   } 
     console.log(operate(15, '/', 3));
     
     function clear() {
       display.textContent = 0 
       numberStored = '' 
-      operatorStored = ''
-    }
+      operatorStored = ''        
+  }
 
     function setDisplay(displayNum) { 
       displayNum = displayNum.toString()
@@ -75,7 +70,7 @@ function add(a, b) {
       } 
       
       display.textContent = displayNum;  
-    } 
+  } 
   
     function addToDisplay(number) {
       let displayNum = display.textContent  
@@ -92,14 +87,14 @@ function add(a, b) {
         displayNum = displayNum + number
         setDisplay(displayNum)
       } 
-    } 
+  } 
 
     buttons.forEach(button =>{ 
       button.addEventListener('click', function(){ 
-          let input = this.textContent 
-                  if (/[0-9\.]/.test(input)) { 
+      let input = this.textContent 
+           if (/[0-9\.]/.test(input)) { 
                                          
-                          if (didEquals) { 
+              if (didEquals) { 
                   setDisplay(input) 
                   didEquals = false 
               } else { 
@@ -111,16 +106,16 @@ function add(a, b) {
                           if (!storedNumber || !clickedOperator) {  
                   alert('Error: no operation entered. Clearing data')  
                   clear() 
-              } else { 
+          } else { 
                   storedNumber = operate(Number(storedNumber), clickedOperator, Number(display.textContent))
                   clickedOperator = ''  
                   setDisplay(storedNumber) 
                   storedNumber = ''
                   didEquals = true  
-              }
-          } 
-          else { 
+          }
+          } else { 
               displayResult(input) 
           } 
       }) 
   }); 
+  operate();
